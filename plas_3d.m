65c0c6fc66a58a9ee443b1a0b886ee90134fc297
GG1991 <giuntoli1991@gmail.com>
2018-05-22 17:38:25 +0200

    adding newtow method in 3d

--- plas_3d.m
+++ plas_3d.m
@@ -10,14 +10,19 @@ K  = E/(3*(1-2*nu));
 M  = (3/2) * (K - L);
 Sy = 5.0e5;
 
-d_eps = 0.0001;
-eps_f = 0.001;
+d_eps = 0.0000001;
+eps_f = 0.00001;
 eps_arr   = [0: d_eps : eps_f];
 time = linspace(0, 10, size(eps_arr,2));
 
 sig_1 = 0;
 eps_p_1 = [0 0 0 0 0 0];
 sig_arr(1) = sig_1;
+dl = 0;
+alpha_1 = 0;
+alpha_2 = 0;
+ka = 1.0e4;
+n = [0 0 0 0 0 0];
 
 for t = 2 : size(eps_arr,2)
 
@@ -25,23 +30,37 @@ for t = 2 : size(eps_arr,2)
 
   eps_2 = [eps_arr(t) 0 0 0 0 0];
   e_2 = eps_2 - (1/3)*(tr(eps_2)) * [1 1 1 0 0 0];
+  e_p_1 = eps_p_1 - (1/3)*(tr(eps_p_1)) * [1 1 1 0 0 0];
 
   eps_p_trial = eps_p_1;
-  s_trial = 2*M*(eps_2 - eps_p_1);
-  f_trial = tn(s_trial) - sqrt(2/3) * (Sy);
+  s_trial = 2*M*(e_2 - e_p_1);
+  f_trial = tn(s_trial) - sqrt(2/3) * (Sy + ka * alpha_2);
+  dl = 0;
 
   if (f_trial < 0)
-    % elastic state
     printf ("LINEAR=YES\n")
+    % elastic state
     s = s_trial;
-    eps_p_2 = eps_p_trial;
   else
-    % return mapping
     printf ("LINEAR = NO\n");
+    % return mapping
+    n = s_trial / tn(s_trial);
+    alpha_2 = alpha_1;
+    its = 0; 
+    do
+      g        = - sqrt(2/3) * ka * alpha_2 + tn(s_trial) - 2*M*dl
+      dg       = - 2*M;
+      dl      -= g/dg;
+      alpha_2 += sqrt(2/3) * dl;
+      its     += 1;
+    until(abs(g)<1.0 || its>4)
+
   end
 
-  %sig_2   = sig_2;
+  eps_p_2 = eps_p_1 + dl*n;
+  sig_2   = K * tr(eps_2) * [1 1 1 0 0 0] + s_trial - 2*M*dl*n;
   eps_p_1 = eps_p_2;
+  alpha_1 = alpha_2;
 
 end
 
--- tn.m
+++ tn.m
@@ -2,7 +2,7 @@ function norm = tn (tensor)
 
 norm = 0;
 for i = 1:6
-   norm += tensor(i);
+   norm += tensor(i)**2;
 endfor
 
 endfunction
